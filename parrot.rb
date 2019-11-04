d17a4e0fbcac9cb1f9ecbd35c8134390967eea17
Kizito Njoku <nkizito57@gmail.com>
2019-11-04 17:40:53 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(noise = "Squawk!")
+<<<<<<< HEAD
   puts noise
   noise
 end
 parrot("Pretty bird!")
+=======
+  puts "Squawk!"
+  noise
+end
+parrot
+>>>>>>> b324f4caba9b7dc2c22982b0e56016f01ff36d0a
